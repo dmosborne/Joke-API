f3ef1e13dcc5a13f7a8d5193b8b1fa53b829a95a
dmosborne <160295634+dmosborne@users.noreply.github.com>
2026-04-30 16:59:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    res.render("index.ejs", {
+        joke: null,
+        user: null
+    });
+});
+
+app.post("/get-joke", async (req, res) => {
+    const username = req.body.username;
+    
+    try {
+        const result = await axios.get("https://v2.jokeapi.dev/joke/Any");
+
+        let joke;
+
+        if (result.data.type === "single") {
+            joke = result.data.joke;
+        } else {
+            joke = `${result.data.setup} - ${result.data.delivery}`;
+        }
+
+        res.render("index.ejs", {
+            joke: joke,
+            user: username
+        });
+
+    } catch (error) {
+        console.log(error.response?.data || error.message);
+        res.status(500).send("Error fetching joke");
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
